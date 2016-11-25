4d1b3378486a235d9e9b1f7b9029b693088fc702
Shaolin <admshao@gmail.com>
2016-11-25 08:10:54 -0200

    Add 2 more IIS properties. c-ip and sc-status
    Signed-off-by: Shaolin <admshao@gmail.com>

--- src/org/nerif/model/InfoPropriedade.java
+++ src/org/nerif/model/InfoPropriedade.java
@@ -1,15 +1,15 @@
 package org.nerif.model;
 
 public enum InfoPropriedade {
-	DATA("DATA"), HORA("HORA"), TEMPO("TEMPO"), URL("URL"), PORTA("PORTA"), TAMANHO(
-			"TAMANHO"), MEDIA("MEDIA");
+	DATA("DATA"), HORA("HORA"), TEMPO("TEMPO"), URL("URL"), PORTA("PORTA"), TAMANHO("TAMANHO"), CLIENT_IP(
+			"CLIENT_IP"), PROTOCOL_STATUS("PROTOCOL_STATUS");
 
 	private final String infoPropriedade;
 
 	InfoPropriedade(String infoPropriedade) {
 		this.infoPropriedade = infoPropriedade;
 	}
-	
+
 	public String getInfoPropriedade() {
 		return infoPropriedade;
 	}
